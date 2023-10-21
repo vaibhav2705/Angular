190f8ec252b575d769d7a5fd0eb19e79e2a51e66
vaibhav2705 <vaibhavsharma143786@gmail.com>
2023-10-21 18:45:55 +0530

    V7.1 Created seller service, used PostAPI and Subscribe the data in seller-auth.component.ts file. 9:00

--- ecomm-project/src/app/app.module.ts
+++ ecomm-project/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { SellerAuthComponent } from './seller-auth/seller-auth.component';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { FormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
